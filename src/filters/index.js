b4c884f0484c69bc01a97c3416450c8df173a602
IlyasDeckers <ilyas@phasehosting.io>
2018-07-18 20:24:58 +0200

    remove unused filters

--- src/filters/index.js
+++ src/filters/index.js
@@ -1,7 +1,4 @@
 /* eslint-disable */
-
-import momentjs from 'moment';
-
 export function json(value) {
   return typeof value === 'string'
     ? value
@@ -37,94 +34,12 @@ export function initials(value) {
   return initials;
 }
 
-export function standardBase(value, type) {
-  if (type === 'supplier') {
-    if (value === 0) {
-      return 'Normale heffing';
-    } else if (value === 3) {
-      return 'IC goederen';
-    } else if (value === 7) {
-      return 'IC diensten';
-    }
-  } else if (type === 'customer') {
-    if (value === 0) {
-      return '0%';
-    } else if (value === 1) {
-      return '6%';
-    } else if (value === 2) {
-      return '12%';
-    } else if (value === 3) {
-      return '21%';
-    } else if (value === 4) {
-      return '0%';
-    } else if (value === 5) {
-      return '0%';
-    } else if (value === 10) {
-      return 'IC diensten';
-    } else if (value === 14) {
-      return 'Niet onderworpen';
-    }
-  }
-  return value;
-}
-
-export function vatSystem(value) {
-  if (value === 0) {
-    return 'Binnenland normaal';
-  } else if (value === 3) {
-    return 'Binnenland vrijgesteld';
-  } else if (value === 4) {
-    return 'EU';
-  } else if (value === 5) {
-    return 'EU + BTW';
-  } else if (value === 6) {
-    return 'Buiten EU';
-  } else if (value === 7) {
-    return 'Buiten EU + BTW';
-  }
-  return value;
-}
-
-export function titlecase(value) {
-  return value
-    .split('_')
-    .map(function(item) {
-      return item.charAt(0).toUpperCase() + item.substring(1);
-    })
-    .join(' ');
-}
-
 export function truncate(text, length, clamp) {
   clamp = clamp || '...';
   let node = document.createElement('div');
   node.innerHTML = text;
   let content = node.textContent;
   return content.length > length ? content.slice(0, length) + clamp : content;
-}
-
-export function time(value) {
-  let hrs = parseInt(value);
-  let min = Math.round((Number(value) - hrs) * 60);
-  if (min === 60) {
-    hrs++;
-    min = 0;
-  }
-
-  hrs = padLeft(hrs, 2, '0');
-  min = padLeft(min, 2, '0');
-  return hrs + ':' + min;
-}
-
-export function percentage(value, total) {
-  return (value / total * 100).toFixed(0);
-}
-
-export function padLeft(val, length, token = ' ') {
-  return `${token.repeat(Math.max(length - String(val).length, 0))}${val}`;
-}
-
-export function padRight(val, length, token = ' ') {
-  return `${val}${token.repeat(Math.max(length - String(val).length, 0))}`;
 }
 
 export function currency(value, decimals) {
@@ -142,12 +57,4 @@ export function number(value, decimals) {
     minimumFractionDigits: decimals,
     maximumFractionDigits: decimals,
   }).format(value);
-}
-
-export function moment(value, format) {
-  format = format || 'MMMM YYYY';
-  return value ? momentjs(value).format(format) : '';
-}
-export function moment_from(value, format) {
-  return value ? momentjs(value).fromNow() : '';
 }
