4ab4357e519ffabc45923da6e54edc5f07a22f4b
IlyasDeckers <ilyas@phasehosting.io>
2018-07-19 16:36:58 +0200

    update config

--- config/dev.env.js
+++ config/dev.env.js
@@ -6,8 +6,9 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   API_URL: '"http://127.0.0.1:8000"',
   PUSHER: {
-    ENABLED: true,
+    ENABLED: false,
     KEY: '"028d91c748f82d72ccd4"',
     CLUSTER: '"eu"'
-  }
+  },
+  FA_PRO: true
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -3,7 +3,9 @@ module.exports = {
   NODE_ENV: '"production"',
   API_URL: '"http://127.0.0.1:8000"',
   PUSHER: {
+    ENABLED: false,
     KEY: '"xxxxx"',
     CLUSTER: '"xx"'
-  }
+  },
+  FA_PRO: true
 }
