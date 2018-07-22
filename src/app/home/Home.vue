3f70abe973b40dd22e88746bcae66d7403af99ac
IlyasDeckers <ilyas@phasehosting.io>
2018-07-22 12:36:15 +0200

    add examples

--- src/app/home/Home.vue
+++ src/app/home/Home.vue
@@ -2,10 +2,10 @@
   <div class="">
     <div class="w-full h-64 bg-blue-light text-center text-white">
       <h1 class="p-4 pt-16 text-white font mono">Vuetiful</h1>
-      <p class="pb-4 text-white">VueJS framework with TailwindCSS and a lot of goodies</p>
-      <router-link to="/docs" class="p-4"><icon :icon="['fas', 'book']" /> Documentation</router-link> |
+      <p class="pb-4 text-white">VueJS framework with TailwindCSS for rapid development</p>
+      <a href="https://ilyasdeckers.github.io/vuetiful-docs/" class="p-4"><fa-icon :icon="['far', 'book']" /> Documentation</a> |
       <a href="https://github.com/ilyasdeckers/vuetiful" class="p-4">
-        <icon :icon="['fab', 'github']" /> GitHub
+        <fa-icon :icon="['fab', 'github']" /> GitHub
       </a>
     </div>
     <div class="mt-16">
@@ -22,6 +22,42 @@
 <div class="text-grey">// Run the documentation on localhost:8081</div>npm run docs:dev
             </code>
           </pre>
+          <div class="my-8">
+            <h3 class="mb-8">Cards</h3>
+            <card title="Card Example">
+              <template slot="card-header">
+                test
+              </template>
+              body
+              <template slot="card-footer">
+                footer
+              </template>
+            </card>
+          </div>
+
+          <div class="my-8">
+            <h3 class="mb-8">Toastr</h3>
+            <button class="btn btn-success" @click.prevent="$toastr.s('Success!')">Success</button>
+            <button class="btn btn-warning" @click.prevent="$toastr.w('Warning!')">Warning</button>
+            <pre class="bg-grey-lighter pl-4 my-4 rounded border">
+              <code>
+$this.toastr.s('text', 'title')
+              </code>
+            </pre>
+          </div>
+          <div class="my-8">
+            <h3 class="mb-8">Calendar Component</h3>
+            <pre class="bg-grey-lighter pl-4 my-4 rounded border">
+              <code>
+import calendar from '@/plugins/calendar/componentsCalendar'
+
+export default {
+  components: { calendar },
+}
+              </code>
+            </pre>
+            <calendar />
+          </div>
         </div>
         <div class="flex-1" />
       </div>
@@ -30,9 +66,12 @@
 </template>
 
 <script>
+import calendar from '@/plugins/calendar/components/Calendar'
+
 export default {
+  components: { calendar },
   created () {
-    console.log(this.$plugin)
+    console.log(this.$test)
   }
 }
 </script>
